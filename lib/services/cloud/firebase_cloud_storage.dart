b23476ff5966508c0e22aa7cb11ba3f83b4e014f
Sagar Sinha <sinhasagar507@gmail.com>
2024-09-06 08:15:40 -0700

    Cloud Storage Stream

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -6,6 +6,24 @@ import 'package:notes_app/services/cloud/cloud_storage_exceptions.dart';
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
+  Stream<Iterable<CloudNote>> allNotes({
+    required String ownerUserId,
+  }) =>
+      notes.snapshots().map((
+            event,
+          ) =>
+              event.docs
+                  .map((
+                    doc,
+                  ) =>
+                      CloudNote.fromSnapshot(
+                        doc,
+                      ))
+                  .where((
+                    note,
+                  ) =>
+                      note.ownerUserId == ownerUserId));
+
   Future<Iterable> getNotes({required String ownerUserId}) async {
     try {
       return await notes
