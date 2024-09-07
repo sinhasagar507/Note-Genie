7d2f07944aa4b07ed917b01e78337dde5402d4f1
Sagar Sinha <sinhasagar507@gmail.com>
2024-09-06 19:08:23 -0700

    step_15

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -6,23 +6,52 @@ import 'package:notes_app/services/cloud/cloud_storage_exceptions.dart';
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
+  // The function 'allNotes' returns a stream of iterable collection objects.
   Stream<Iterable<CloudNote>> allNotes({
-    required String ownerUserId,
+    required String
+        ownerUserId, // The ID of the user whose notes are to be retrieved
   }) =>
       notes.snapshots().map((
-            event,
+            // Receives a stream of snapshots from the 'notes' collection
+            event, // Each snapshot contains the current state of the notes collection
           ) =>
-              event.docs
+              event.docs // Access the list of documents from the snapshot
                   .map((
-                    doc,
+                    doc, // For each document in the snapshot
                   ) =>
                       CloudNote.fromSnapshot(
+                        // Convert the document data into a CloudNote object
                         doc,
                       ))
                   .where((
-                    note,
+                    note, // Filter the CloudNote objects
                   ) =>
-                      note.ownerUserId == ownerUserId));
+                      note.ownerUserId ==
+                      ownerUserId)); // Only return notes that belong to the specified ownerUserId
+
+  // The following function is used to 'update' a specific note
+  Future<void> updateNotes({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
+      await notes.doc(documentId).update({
+        // From the 'notes' collection, retrieve the document with the appropriate document ID through the notes/document ID path
+        textFieldName: text, // Update the document with the given 'text'
+      });
+    } catch (_) {
+      throw CouldNotUpdateNoteException();
+    }
+  }
+
+  // The following function is used to 'delete' a specific note
+  Future<void> deleteNote({required String documentId}) async {
+    try {
+      await notes.doc(documentId).delete();
+    } catch (_) {
+      throw CouldNotDeleteNoteException();
+    }
+  }
 
   Future<Iterable> getNotes({required String ownerUserId}) async {
     try {
